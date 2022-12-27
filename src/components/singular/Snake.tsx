085cbfcbb6e46d72e8fc7a264da525abda5c74bc
Rares Popa <rares_teodor@yahoo.ro>
2022-12-27 22:56:09 +0200

    .

--- src/components/singular/Snake.tsx
+++ src/components/singular/Snake.tsx
@@ -1,16 +1,37 @@
 import React from "react";
 import NormalApple from "../../resources/png/apple_normal.png";
+import GoldenApple from "../../resources/png/apple_golden.png";
+
+enum AppleTypes {
+    Normal,
+    Golden
+}
+
+interface AppleProps {
+    typeOfApple: AppleTypes,
+    value: number
+}
+
+const Apple:React.FC<AppleProps> = (props) => {
+    return (<div className="relative select-none w-full h-full flex items-center justify-center">
+        <img src={props.typeOfApple === AppleTypes.Normal ? NormalApple : GoldenApple} 
+        alt="" className="select-none unselectable-image relative w-[80%] h-[80%] text-red-600"/>
+        <div className="absolute text-white font-semibold text-[75%] mr-[3px] mt-[2px]">+{props.value}</div>
+    </div>);
+}
+
+const boardSize:number = 11;
 
 const Snake:React.FC = () => {
     return (<div className="w-full h-full aspect-square bg-black">
-        <div className="grid w-full h-full grid-cols-[repeat(11,minmax(0,1fr))] grid-rows-[repeat(11,minmax(0,1fr))] 
-        [&>*:nth-child(even)]:bg-green-500 [&>*:nth-child(odd)]:bg-emerald-700 shadow-2xl">
-            {[...Array(120)].map(() => <div/>)}
-            <div>
-                <div className="w-full h-full flex items-center justify-center">
-                    <img src={NormalApple} alt="" className="w-[80%] h-[80%] text-red-600"/>
-                </div>
-            </div>
+        <div 
+        style={{
+            gridTemplateColumns: 'repeat(' + boardSize + ', minmax(0, 1fr))',
+            gridTemplateRows: 'repeat(' + boardSize + ', minmax(0, 1fr))'
+        }}
+        className="grid w-full h-full shadow-2xl
+        [&>*:nth-child(even)]:bg-green-500 [&>*:nth-child(odd)]:bg-emerald-700">
+            {[...Array(boardSize * boardSize)].map(() => <div/>)}
         </div>
     </div>);
 }
