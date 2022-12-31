4ae322940912e3efc77e764e186306f1bebb463d
Rares Popa <rares_teodor@yahoo.ro>
2022-12-31 22:57:00 +0200

    Bug fixes

--- src/components/singular/Snake.tsx
+++ src/components/singular/Snake.tsx
@@ -341,11 +341,11 @@ const Snake:React.FC = () => {
 
         {pauseTime !== 0 && <div 
         style={{opacity: Math.min(1, pauseTime / pauseTimeStart + 0.3)}}
-        className="absolute top-0 w-full h-full flex items-center justify-center z-[60]">
+        className="absolute top-0 w-full h-full z-[60]">
             <div className="text-white font-bold text-[120px]">{pauseTime}{pauseTime === Math.floor(pauseTime) ? ".0" : ""}</div>
         </div>}
 
-        {!isGameRunning && <button className="absolute top-0 w-full h-full bg-black bg-opacity-40 z-[60]
+        {!isGameRunning && <button className="absolute top-[-0.5px] w-[calc(100%+0.5px)] h-[calc(100%+0.5px)] bg-black bg-opacity-40 z-[60]
         flex items-center justify-center"
         onClick={handleStartGame}>
             <BsTriangleFill className="rotate-90 w-1/5 h-1/5 text-white text-opacity-80"/>
