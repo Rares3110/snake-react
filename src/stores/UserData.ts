c78d10378dff7aec1368b4288d12894a12cd93c4
Rares Popa <rares_teodor@yahoo.ro>
2023-01-02 13:42:09 +0200

    Mobx persist

--- src/stores/UserData.ts
+++ src/stores/UserData.ts
@@ -1,11 +1,28 @@
 import { makeAutoObservable } from "mobx";
 import { User } from "firebase/auth";
+import { makePersistable } from 'mobx-persist-store';
 
 class UserData {
     user: User | null = null;
 
     constructor() {
         makeAutoObservable(this);
+
+        makePersistable(this, {
+            name: 'UserDataStore',
+            properties: [
+                {
+                    key: 'user',
+                    serialize: (value) => {
+                        return value;
+                    },
+                    deserialize: (value) => {
+                        return value;
+                    },
+                },
+            ],
+            storage: window.localStorage,
+        });
     }
 
     setUser(user: User) {
