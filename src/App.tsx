d3d143c463bdef62075bbe23a3dc87f343960a96
Rares Popa <rares_teodor@yahoo.ro>
2023-01-05 00:51:24 +0200

    Get history function

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,3 @@
-import React from "react";
 import { Routes, Route, Navigate } from "react-router-dom";
 import AccountPage from "./components/pages/AccountPage";
 import FullSizeSnakePage from "./components/pages/FullSizeSnakePage";
--- src/services/UserInfo.ts
+++ src/services/UserInfo.ts
@@ -2,7 +2,7 @@ import { getDownloadURL, ref, uploadBytes } from "firebase/storage";
 import userData from "../stores/UserData";
 import {auth, firestoreDB, storage} from "./FirebaseConnection";
 import { updateProfile } from "firebase/auth";
-import { addDoc, collection, doc, setDoc, Timestamp } from "firebase/firestore";
+import { addDoc, collection, doc, DocumentData, getDocs, limit, orderBy, query, QueryDocumentSnapshot, setDoc, startAfter, Timestamp } from "firebase/firestore";
 
 export const changeIcon = async(icon: File):Promise<boolean> => {
     if(userData.id !== undefined) {
@@ -47,5 +47,46 @@ export const addGame = (score: number, seconds: number) => {
                     gamesPlayed: userData.gamesPlayed
                 });
         });
+    }
+}
+
+var lastHistoryRecord: QueryDocumentSnapshot<DocumentData>;
+var historyNotFull: boolean;
+export const getHistory = async(initial: boolean = true) => {
+    if(userData.id !== undefined && initial) {
+
+        return getDocs(query(
+            collection(firestoreDB, 'users/' + userData.id + '/scores'), 
+            orderBy("date", "desc"),
+            limit(10)
+        )).then((docSnap) => {
+            if(docSnap.docs.length > 0) {
+                lastHistoryRecord = docSnap.docs[docSnap.docs.length - 1];
+                historyNotFull = true;
+                return docSnap.docs.map(doc => doc.data());
+            } else {
+                return [];
+            }
+        });
+
+    } else if(userData.id !== undefined && historyNotFull) {
+
+        return getDocs(query(
+            collection(firestoreDB, 'users/' + userData.id + '/scores'), 
+            orderBy("date", "desc"),
+            startAfter(lastHistoryRecord),
+            limit(10)
+        )).then((docSnap) => {
+            if(docSnap.docs.length > 0) {
+                lastHistoryRecord = docSnap.docs[docSnap.docs.length - 1];
+                return docSnap.docs.map(doc => doc.data());
+            } else {
+                historyNotFull = false;
+                return [];
+            }
+        });
+
+    } else {
+        return [];
     }
 }
